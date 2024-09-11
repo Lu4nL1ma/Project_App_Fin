76dceddfa0b2546737bd94b9f8a6eedb4a572f4b
Lu4nL1ma <luanlima.corp@gmail.com>
2024-09-10 22:00:01 -0300

    return_test

--- fin_tech/fin_tech/settings.py
+++ fin_tech/fin_tech/settings.py
@@ -127,7 +127,3 @@ STATIC_ROOT = BASE_DIR / 'fin_tech/financas_online/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-test the very important!
-test the very important!
-test the very important!
