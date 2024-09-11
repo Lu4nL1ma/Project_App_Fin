7ebc7950d9dd2738d1c493b0bffd83db7286d06b
Lu4nL1ma <luanlima.corp@gmail.com>
2024-09-11 13:45:25 +0000

    new_changes

--- fin_tech/financas_online/tests.py
+++ fin_tech/financas_online/tests.py
@@ -1,6 +1,27 @@
 from django.test import TestCase
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import numpy as np
+# import sqlite3
 
 # Create your tests here.
-print("Hello Porra!")
+
+# conection = sqlite3.connect('fin_tech\\db.sqlite3')
+
+# cursor = conection.cursor()
+
+# #fim = ["django_session"]
+
+# fim = np.arange(1058,1077+1,1)
+
+# for i in fim:
+
+#     delete_query = f'DELETE FROM financas_online_financas WHERE id={i}'
+
+#     #delete_query = f'DROP TABLE IF EXISTS {i}'
+
+#     cursor.execute(delete_query)
+
+# conection.commit()
+
+# conection.close()
