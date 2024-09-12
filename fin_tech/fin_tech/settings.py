098cbe5dabb9056350e07696f9d6fb687cdc5028
Lu4nL1ma <luanlima.corp@gmail.com>
2024-09-11 23:40:12 -0300

    broken_seting

--- fin_tech/fin_tech/settings.py
+++ fin_tech/fin_tech/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'fin_tech.urls'
 
 TEMPLATES = [
     {
-        # 'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['/home/Lu4nL1ma/meu_aplicativo/Project_App_Fin/fin_tech/financas_online/Templates'],
         # 'DIRS': ['Templates'],
         'APP_DIRS': True,
