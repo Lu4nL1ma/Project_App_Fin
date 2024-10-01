894ebd3617d8b7a11bc1c8a851afeac268b774b6
Lu4nL1ma <luanlima.corp@gmail.com>
2024-10-01 12:30:16 +0000

    return_db

--- fin_tech/fin_tech/settings.py
+++ fin_tech/fin_tech/settings.py
@@ -80,14 +80,22 @@ WSGI_APPLICATION = 'fin_tech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Lu4nL1ma$finappstech',
-        'USER': 'Lu4nL1ma',
-        'PASSWORD': 'luansql!',
-        'HOST': 'Lu4nL1ma.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Lu4nL1ma$finappstech',
+#         'USER': 'Lu4nL1ma',
+#         'PASSWORD': 'luansql!',
+#         'HOST': 'Lu4nL1ma.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
