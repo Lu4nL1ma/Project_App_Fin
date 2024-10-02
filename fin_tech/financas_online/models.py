fef4e46027ab0a6162c286415b2d757f580aff7c
Lu4nL1ma <luanlima.corp@gmail.com>
2024-10-02 15:00:12 -0300

    register

--- fin_tech/financas_online/models.py
+++ fin_tech/financas_online/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class cursos(models.Model):
     curso = models.CharField(max_length=50, default='não informado!')
-    registro = models.DateField(auto_now=True)
+    registro_curs = models.DateField(auto_now=True)
     def __str__(self) -> str:
         return self.curso
 
 class turmas_formatec(models.Model):
     turmas_formatec = models.CharField(max_length=40, default='não informado!')
-    registro = models.DateField(auto_now=True)
+    registro_turms = models.DateField(auto_now=True)
     def __str__(self) -> str:
         return self.turmas_formatec
 
@@ -19,7 +19,7 @@ class customers(models.Model):
     cpf = models.CharField(max_length=50, blank=False, default='não informado!')
     telefone = models.CharField(max_length=50, blank=False, default='não informado!')
     nascimento = models.DateField(auto_now=False, blank=False)
-    registro = models.DateField(auto_now=True)
+    registro_client = models.DateField(auto_now=True)
 
 class financas(models.Model):
     id_ori = models.CharField(max_length=50, blank=False, default='não informado!')
@@ -32,7 +32,7 @@ class financas(models.Model):
     data_pagamento = models.DateField(auto_now=False, blank=False)
     banco = models.CharField(max_length=50, blank=False, default='não informado!')
     arquivo = models.FileField(upload_to='posts/%Y/%m/%d/', blank=True, default='não informado!')
-    registro = models.DateField(auto_now=True)
+    registro_fin = models.DateField(auto_now=True)
 
     
 
