e72eddfa4c0e2d1f061df7810dc3cc343dd0fa98
Lu4nL1ma <luanlima.corp@gmail.com>
2024-10-02 13:39:07 +0000

    model_default

--- fin_tech/fin_tech/settings.py
+++ fin_tech/fin_tech/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'fin_tech.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Lu4nL1ma$dates_fin',
+        'NAME': 'Lu4nL1ma$default',
         'USER': 'Lu4nL1ma',
         'PASSWORD': 'luansql!',
         'HOST': 'Lu4nL1ma.mysql.pythonanywhere-services.com',
