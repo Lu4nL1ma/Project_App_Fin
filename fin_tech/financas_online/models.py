30216a4801bce5c67052088fcf92cafb65b9d8e9
Lu4nL1ma <luanlima.corp@gmail.com>
2024-10-02 13:11:50 +0000

    model_default

--- fin_tech/financas_online/models.py
+++ fin_tech/financas_online/models.py
@@ -3,33 +3,36 @@ from django.db import models
 # Create your models here.
 
 class cursos(models.Model):
-    curso = models.CharField(max_length=50)
+    curso = models.CharField(max_length=50, default='não informado!')
+    registro = models.DateField(auto_now=True), default='não informado!'
     def __str__(self) -> str:
         return self.curso
 
 class turmas_formatec(models.Model):
-    turmas_formatec = models.CharField(max_length=40)
+    turmas_formatec = models.CharField(max_length=40, default='não informado!')
+    registro = models.DateField(auto_now=True), default='não informado!'
     def __str__(self) -> str:
         return self.turmas_formatec
 
 class customers(models.Model):
-    nome = models.CharField(max_length=100, blank=False)
-    cpf = models.CharField(max_length=50, blank=False)
-    telefone = models.CharField(max_length=50, blank=False)
-    nascimento = models.DateField(auto_now=False)
-    registro = models.DateField(auto_now=True)
+    nome = models.CharField(max_length=100, blank=False, default='não informado!')
+    cpf = models.CharField(max_length=50, blank=False), default='não informado!'
+    telefone = models.CharField(max_length=50, blank=False, default='não informado!')
+    nascimento = models.DateField(auto_now=False, default='não informado!')
+    registro = models.DateField(auto_now=True), default='não informado!'
 
 class financas(models.Model):
-    id_ori = models.CharField(max_length=50, blank=False)
-    cliente = models.CharField(max_length=100, blank=False)
-    parcela = models.CharField(max_length=100, blank=False)
-    valor = models.CharField(max_length=100, blank=False)
-    curso = models.CharField(max_length=50, blank=False)
-    turma = models.CharField(max_length=50,blank=False)
-    data = models.DateField(auto_now=False)
-    data_pagamento = models.DateField(auto_now=False)
-    banco = models.CharField(max_length=50,blank=False)
-    arquivo = models.FileField(upload_to='posts/%Y/%m/%d/', blank=True)
+    id_ori = models.CharField(max_length=50, blank=False, default='não informado!')
+    cliente = models.CharField(max_length=100, blank=False, default='não informado!')
+    parcela = models.CharField(max_length=100, blank=False, default='não informado!')
+    valor = models.CharField(max_length=100, blank=False, default='não informado!')
+    curso = models.CharField(max_length=50, blank=False, default='não informado!')
+    turma = models.CharField(max_length=50,blank=False, default='não informado!')
+    data = models.DateField(auto_now=False, default='não informado!')
+    data_pagamento = models.DateField(auto_now=False, default='não informado!')
+    banco = models.CharField(max_length=50,blank=False, default='não informado!')
+    arquivo = models.FileField(upload_to='posts/%Y/%m/%d/', blank=True, default='não informado!')
+    registro = models.DateField(auto_now=True), default='não informado!'
 
     
 
