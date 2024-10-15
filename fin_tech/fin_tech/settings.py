5d9c1cdcebb81203ef9f4fa7047d60baec4b1ae7
Lu4nL1ma <luanlima.corp@gmail.com>
2024-10-14 23:05:48 -0300

    ndb

--- fin_tech/fin_tech/settings.py
+++ fin_tech/fin_tech/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'fin_tech.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Lu4nL1ma$for_tec_db',
+        'NAME': 'Lu4nL1ma$nova_base_formatec',
         'USER': 'Lu4nL1ma',
         'PASSWORD': 'luansql!',
         'HOST': 'Lu4nL1ma.mysql.pythonanywhere-services.com',
