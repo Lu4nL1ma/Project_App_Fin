f359159027b69091bc89f6b22449db3aea78f714
Lu4nL1ma <luanlima.corp@gmail.com>
2024-10-30 12:01:14 +0000

    dinamics

--- fin_tech/financas_online/views.py
+++ fin_tech/financas_online/views.py
@@ -148,8 +148,8 @@ def customer(request, c_id):
          f_nome = l.nome.split()[0]
          l_nome = l.nome.split()[-1]
          numero = l.telefone
-         msg = f'Olá, {f_nome} {l_nome}, somos da Formatec, e você têm o débito abaixo pendente:'
-         link_wpp = f'https://web.whatsapp.com/send/?phone=55{numero}&text={quote(msg)}%0A'
+         msg = f'Olá, {f_nome} {l_nome}, somos da Formatec, e segue informações do bébito com status de {fin.status}:'
+         link_wpp = f'https://web.whatsapp.com/send/?phone=55{numero}&text={quote(msg)}%0A%0A'
 
       context = {'c': id_cliente,'fin': fin, 'url': url_arquivo, 'link_wpp': link_wpp}
       
