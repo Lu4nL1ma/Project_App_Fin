c401a9cdaf93c1aa1c22cd9d3ba072e91c1ef0a5
Lu4nL1ma <luanlima.corp@gmail.com>
2024-11-13 15:48:07 -0300

    pay17

--- fin_tech/fin_tech/settings.py
+++ fin_tech/fin_tech/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#caminho templates
+
+cam = os.path.join(BASE_DIR, 'financas_online')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -58,7 +61,8 @@ ROOT_URLCONF = 'fin_tech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/Lu4nL1ma/meu_aplicativo/Project_App_Fin/fin_tech/financas_online/Templates'],
+        # 'DIRS': ['/home/Lu4nL1ma/meu_aplicativo/Project_App_Fin/fin_tech/financas_online/Templates'],
+        'DIRS': [f'{cam}\\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
